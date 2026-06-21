30ea28151bb6400eeb28bf1370d11ac5cc08762b
devamirhub <devamir99@gmail.com>
2026-06-21 15:07:28 +0330

    chore(config): allow picsum.photos for next/image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
